5f1ccde50bf71496a4e682e16eb1571d968d1c02
abrilmayab697-code <abrilmayab697@gmail.com>
2026-02-03 13:09:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,2 @@
-document.addEventListener("DOMContentLoaded", () => {
 
-  const eventos = [
-    "1950 – Test de Turing",
-    "1957 – Perceptrón",
-    "1997 – Deep Blue",
-    "2012 – Deep Learning",
-    "2020+ – IA generativa"
-  ];
-
-  let indice = 0;
-  const timeline = document.getElementById("timeline");
-
-  // Seguridad extra
-  if (!timeline) {
-    console.error("No existe el elemento con id 'timeline'");
-    return;
-  }
-
-  function cambiarEvento() {
-    timeline.textContent = eventos[indice];
-    indice = (indice + 1) % eventos.length;
-  }
-
-  cambiarEvento();
-  setInterval(cambiarEvento, 2500);
-
-});
 
