918c27d997cf793958e9d231631cb1f2f337d1c9
abrilmayab697-code <abrilmayab697@gmail.com>
2026-02-10 12:49:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const form = document.getElementById("contacto");
-  const modal = document.getElementById("modal");
-  const cerrar = document.getElementById("cerrar");
 
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();       // Evita que la página se recargue
-    modal.style.display = "flex"; // Muestra el modal
-  });
-
-  cerrar.addEventListener("click", () => {
-    modal.style.display = "none"; // Cierra el modal al pulsar
-    form.reset();                 // Opcional: limpia los campos del formulario
-  });
-});
